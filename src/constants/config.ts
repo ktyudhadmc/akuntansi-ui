6eee5c8ada9761f430cbf11c290a64703b54f423
ktyudha <ktyudha@gmail.com>
2026-01-16 03:16:21 +0700

    feat: add sentry & ga

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -6,6 +6,7 @@ export const BASE_API_URL =
   import.meta.env.VITE_BASE_API_URL ?? DEFAULT_API_URL;
 
 export const SENTRY_DSN = import.meta.env.VITE_SENTRY_DSN;
+export const GA_ID = import.meta.env.VITE_GA_ID;
 
 export const LOCAL_STORAGE_COMPANY_KEY = import.meta.env.VITE_LOCAL_STORAGE_COMPANY_KEY ?? "active-company";
 
@@ -13,6 +14,7 @@ const config = {
   APP_NAME,
   BASE_API_URL,
   SENTRY_DSN,
+  GA_ID,
   LOCAL_STORAGE_COMPANY_KEY,
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import { StrictMode, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import * as Sentry from "@sentry/react";
 
+import * as Sentry from "@sentry/react";
 
 import { AppWrapper } from "@components/common/PageMeta.tsx";
 import { ThemeProvider } from "@context/ThemeContext.tsx";
+import Loader from "@components/Reusable/Loader";
+import { initAnalytics, subscribeGA } from "@constants/analytics";
 
 import GetBrowserRoutes from "./routes/index.routes";
 
@@ -15,29 +17,18 @@ import "./styles/index.css";
 import "swiper/swiper-bundle.css";
 import "flatpickr/dist/flatpickr.css";
 import "react-toastify/dist/ReactToastify.css";
-import Loader from "@components/Reusable/Loader";
-import config from "@constants/config";
 
-const apiRegex = new RegExp(
-  config.BASE_API_URL.replace(/[.*+?^${}()|[\]\\]/g, "\\$&")
-);
 
-Sentry.init({
-  dsn: config.SENTRY_DSN,
-  integrations: [Sentry.browserTracingIntegration()],
-  // Setting this option to true will send default PII data to Sentry.
-  // For example, automatic IP address collection on events
-  sendDefaultPii: true,
-  tracesSampleRate: 1.0,
-  tracePropagationTargets: ["localhost", apiRegex],
-});
 
-// const router = createBrowserRouter(GetBrowserRoutes());
+initAnalytics();
 
 const sentryCreateBrowserRouter =
   Sentry.wrapCreateBrowserRouterV6(createBrowserRouter);
 
 const router = sentryCreateBrowserRouter(GetBrowserRoutes());
+
+subscribeGA(router);
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
