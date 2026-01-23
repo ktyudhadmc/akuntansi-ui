6d882101e2e13de67abcc935c0ea975764b519d0
ktyudha <ktyudha@gmail.com>
2026-01-23 14:33:47 +0700

    feat: add popover component & impl to confirm delete unit

--- src/components/ui/popover/index.tsx
+++ src/components/ui/popover/index.tsx
@@ -1,43 +1,54 @@
-export default function PopOver() {
+import {
+  useFloating,
+  offset,
+  flip,
+  shift,
+  autoUpdate,
+} from "@floating-ui/react";
+
+import { useEffect } from "react";
+
+interface Props {
+  onOpen: boolean;
+  onClose: () => void;
+  reference: HTMLElement | null;
+  children: React.ReactNode;
+}
+
+export default function PopOver({
+  onOpen,
+  onClose,
+  reference,
+  children,
+}: Props) {
+  const { refs, floatingStyles, update } = useFloating({
+    middleware: [offset(16), flip(), shift({ padding: 20 })],
+  });
+
+  // pasang anchor
+  useEffect(() => {
+    if (reference) refs.setReference(reference);
+  }, [reference]);
+
+  // auto reposition saat scroll / resize
+  useEffect(() => {
+    if (!reference || !onOpen) return;
+
+    return autoUpdate(reference, refs.floating.current!, update);
+  }, [onOpen, reference]);
+
+  if (!onOpen) return null;
+
   return (
     <>
+      <div className="fixed inset-0 z-[100001]" onClick={onClose} />
+
       <div
-        className="absolute z-99999 bg-white border border-gray-200 rounded-xl dark:bg-[#1E2634] dark:border-gray-700 block"
-        // style={"left: 548.891px; top: 509px;"}
+        ref={refs.setFloating}
+        style={floatingStyles}
+        className={`absolute z-[100001] bg-white border border-gray-200 rounded-lg dark:bg-[#1E2634] dark:border-gray-700 ${onOpen ? "block" : "hidden"}`}
       >
-        <div className="max-w-[390px]">
-          <div className="relative z-20 rounded-t-xl border-b border-gray-200 px-5 py-3 dark:border-white/[0.03]">
-            <h4 className="text-base font-semibold text-gray-800 dark:text-white/90">
-              Popover Title
-            </h4>
-          </div>
-          <div className="p-5">
-            <p className="text-sm text-gray-500 dark:text-gray-400">
-              Lorem ipsum dolor sit amet, consect adipiscing elit. Mauris
-              facilisis congue exclamate justo nec facilisis.
-            </p>
-            <div className="mt-5 flex items-center gap-3">
-              <button
-                type="button"
-                className="bg-brand-500 shadow-theme-xs hover:bg-brand-600 flex justify-center rounded-lg px-4 py-2 text-sm font-medium text-white"
-              >
-                Yes! got it
-              </button>
-
-              <button
-                type="button"
-                className="shadow-theme-xs flex justify-center rounded-lg border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 hover:text-gray-800 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.03] dark:hover:text-gray-200"
-              >
-                Learn more
-              </button>
-            </div>
-          </div>
-        </div>
-
-        <div
-          className="popover-arrow absolute h-3 w-3 rotate-45 bg-white dark:bg-[#1E2634] border-l border-t border-gray-200 dark:border-gray-700"
-          //   style="left: 189px; top: -6px;"
-        ></div>
+        {children}
       </div>
     </>
   );
--- src/modules/user/dashboard/index.tsx
+++ src/modules/user/dashboard/index.tsx
@@ -1,29 +1,7 @@
-// import Button from "@components/ui/button/Button";
-// import { useSetCurrentCompany } from "@services/auth/hooks/useSetCurrentCompany";
-// import { useUnsetCurrentCompany } from "@services/auth/hooks/useUnsetCurrentCompany";
-// import useGlobalStore from "@store/useStore";
-
 export default function Dashboard() {
-  // const { setCompany } = useSetCurrentCompany();
-  // const { unsetCompany } = useUnsetCurrentCompany();
-  // const currentCompany = useGlobalStore((state) => state.currentCompany);
-  // const isSelectCompany = useGlobalStore((state) => state.isSelectCompany);
-
   return (
     <div>
       <h1 className="text-gray-500 dark:text-gray-400">Dashboard, It Works!</h1>
-      {/* <p>{currentCompany?.id}</p>
-      <p>{currentCompany?.code}</p>
-      <p>{currentCompany?.name}</p>
-      <p>{currentCompany?.email}</p>
-      <p>{currentCompany?.phone}</p>
-      <p>{currentCompany?.address}</p>
-
-      <p>select: {isSelectCompany? "true": "false"}</p>
-      <Button onClick={() => setCompany("1")}>Company 1</Button>
-      <Button onClick={() => unsetCompany()} variant="outline">
-        Unset
-      </Button> */}
     </div>
   );
 }
