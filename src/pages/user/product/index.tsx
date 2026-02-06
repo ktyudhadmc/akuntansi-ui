c740fb44640c45a45e7704cd7cdf283b49186767
ktyudha <ktyudha@gmail.com>
2026-02-06 14:37:07 +0700

    feat: adjustment card header

--- src/pages/user/product/index.tsx
+++ src/pages/user/product/index.tsx
@@ -10,12 +10,14 @@ export default function ProductPage() {
       <PageMeta title={pageTitle} />
       <PageBreadcrumb pageTitle={pageTitle} />
 
-      <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] lg:p-6">
-        <div className="space-y-6">
-          <ProductHeader />
-
-          {/* <h4>Halaman Produk, It Works!</h4> */}
-          <ProductTable />
+      <div className="space-y-6">
+        <ProductHeader />
+        
+        <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] lg:p-6">
+          <div className="space-y-6">
+            {/* <h4>Halaman Produk, It Works!</h4> */}
+            <ProductTable />
+          </div>
         </div>
       </div>
     </>
