1b77b8f9d7d8dd222a21e7dbacf29714cb9b9420
ktyudha <ktyudha@gmail.com>
2026-02-12 13:47:07 +0700

    feat: implement useClose period

--- src/modules/user/period/actions/period/Lock.tsx
+++ src/modules/user/period/actions/period/Lock.tsx
@@ -1,17 +1,19 @@
+import { toast } from "react-toastify";
+import { useForm, type SubmitHandler } from "react-hook-form";
+
+import { todayYMString } from "@helpers/date";
+import useGoBack from "@hooks/useGoBack";
+
 import DatePicker from "@components/form/date-picker";
 import Form from "@components/form/Form";
 import Checkbox from "@components/form/input/Checkbox";
-
 import Spinner from "@components/Reusable/Spinner";
 import Alert from "@components/ui/alert";
 import Button from "@components/ui/button/Button";
-import { todayYMDString } from "@helpers/date";
-import useGoBack from "@hooks/useGoBack";
-import { useForm, type SubmitHandler } from "react-hook-form";
+import useClose from "@services/user/period/hooks/useClose";
 
-// import { toast } from "react-toastify";
-
-type FormFields = { start_date: string; end_date: string; accept: boolean };
+// type FormFields = { start_date: string; end_date: string; accept: boolean };
+type FormFields = { period_date: string; accept: boolean };
 
 export default function Lock() {
   const goBack = useGoBack();
@@ -20,22 +22,25 @@ export default function Lock() {
   const { isSubmitting } = methods.formState;
   const isValid = methods.formState.isValid;
 
-  const onSubmit: SubmitHandler<FormFields> = async (state) => {
-    console.log({ ...state, start_date: todayYMDString });
-    // const { error, response } = await closeData({
-    //   year: Number(year),
-    //   month: Number(month),
-    // });
+  const { closeData } = useClose();
 
-    // if (error || response) {
-    //   if (error) {
-    //     toast.error("Gagal menutup periode!");
-    //   } else {
-    //     methods.reset();
-    //     goBack();
-    //     toast.success("Berhasil menutup periode!");
-    //   }
-    // }
+  const onSubmit: SubmitHandler<FormFields> = async (state) => {
+    const [year, month] = state.period_date.split("-");
+
+    const { error, response } = await closeData({
+      year: Number(year),
+      month: Number(month),
+    });
+
+    if (error || response) {
+      if (error) {
+        toast.error("Gagal menutup periode!");
+      } else {
+        methods.reset();
+        goBack();
+        toast.success("Berhasil menutup periode!");
+      }
+    }
   };
   return (
     <div className="lg:max-w-md mx-auto">
@@ -46,17 +51,25 @@ export default function Lock() {
 
       <Form {...methods} onSubmit={onSubmit} className="mt-6">
         <div className="flex flex-col gap-8">
-          <DatePicker
+          {/* <DatePicker
             label="Tgl. mulai"
             name="start_date"
             defaultValue={todayYMDString}
             required
             disabled
-          />
-          <DatePicker
+          /> */}
+          {/* <DatePicker
             label="Tgl. selesai"
             name="end_date"
             defaultValue={todayYMDString}
+            mode="month"
+            required
+          /> */}
+          <DatePicker
+            label="Tgl. Kunci Periode"
+            name="period_date"
+            defaultValue={todayYMString}
+            mode="month"
             required
           />
 
