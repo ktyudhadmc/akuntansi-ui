06e8dad6935b43d13caa95f0c607fcdf9bdf944c
ktyudha <ktyudha@gmail.com>
2026-02-12 13:24:11 +0700

    feat: initial chart

--- src/modules/user/dashboard/index.tsx
+++ src/modules/user/dashboard/index.tsx
@@ -1,29 +1,22 @@
-// import TablePagination from "@components/ui/table/TablePagination";
-// import usePagination from "@hooks/usePagination";
-// import { useState } from "react";
+import ChartPurchase from "./chart/ChartPurchase";
+import ChartSale from "./chart/ChartSale";
 
 export default function Dashboard() {
-  // const lastPage = 10;
-
-  // const [pageLimit, setPageLimit] = useState(10);
-  // const { currentPage, goNextPage, goPrevPage, setPageNum } =
-  //   usePagination(lastPage);
-
   return (
     <div>
-      <h1 className="text-gray-500 dark:text-gray-400">Dashboard, It Works!</h1>
+      {/* <h1 className="text-gray-500 dark:text-gray-400">Dashboard, It Works!</h1> */}
 
-      {/* <TablePagination
-        goNextPage={goNextPage}
-        goPrevPage={goPrevPage}
-        setPageNum={setPageNum}
-        // perPage={10}
-        total={100}
-        pageLimit={pageLimit}
-        setPageLimit={(limit) => setPageLimit(limit)}
-        currentPage={currentPage}
-        lastPage={lastPage}
-      /> */}
+      {/* <BarChartOne />
+      <LineChartOne /> */}
+
+      <div className="grid lg:grid-cols-2 grid-cols-1 gap-6">
+        <div>
+          <ChartSale />
+        </div>
+        <div>
+          <ChartPurchase />
+        </div>
+      </div>
     </div>
   );
 }
