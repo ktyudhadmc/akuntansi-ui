b1c61c26390aa05279bc3f413b4366a0968eda4b
ktyudha <ktyudha@gmail.com>
2026-02-13 13:54:22 +0700

    feat: change response trial balance

--- src/services/user/report/trial-balance/interfaces/response.type.ts
+++ src/services/user/report/trial-balance/interfaces/response.type.ts
@@ -1,14 +1,26 @@
 export interface IGetAllTrialBalanceResponse {
   status?: string;
   message?: string;
-  data: TrialBalance[];
+  data: TrialBalance;
 }
 
 export interface TrialBalance {
+  total: TrialBalanceTotal;
+  accounts: TrialBalanceAccount[];
+}
+
+export interface TrialBalanceTotal {
+  opening_balance: CashFlow;
+  movement_balance: CashFlow;
+  closing_balance: CashFlow;
+}
+
+export interface TrialBalanceAccount {
   id: string;
   name: string;
   code: string;
   opening_balance: CashFlow;
+  movement_balance: CashFlow;
   closing_balance: CashFlow;
 }
 
