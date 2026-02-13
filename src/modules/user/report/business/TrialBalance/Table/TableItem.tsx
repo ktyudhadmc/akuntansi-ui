210c4f9a2c3e9217d5216b6ae7d3ff8e8edebcb0
ktyudha <ktyudha@gmail.com>
2026-02-13 14:18:48 +0700

    feat: implement report trial balance

--- src/modules/user/report/business/TrialBalance/Table/TableItem.tsx
+++ src/modules/user/report/business/TrialBalance/Table/TableItem.tsx
@@ -1,35 +1,36 @@
 import { formatIDRLocale } from "@helpers/currency";
+import type { TrialBalanceAccount } from "@services/user/report/trial-balance/interfaces/response.type";
 
 interface Props {
-  nomor: number;
+  item: TrialBalanceAccount;
 }
 
-export default function TableItem({ nomor }: Props) {
+export default function TableItem({ item }: Props) {
   return (
     <tr>
       <td className="pl-5 py-1 text-black text-start text-theme-xs dark:text-white whitespace-nowrap">
-        1-1000{nomor}
+        {item.code}
       </td>
       <td className="px-5 py-1 text-black text-start text-theme-xs dark:text-white whitespace-nowrap border-r dark:border-white/[0.10]">
-        Kas
+        {item.name}
       </td>
       <td className="px-5 py-1 text-black text-end text-theme-xs dark:text-white whitespace-nowrap border-r dark:border-white/[0.10]">
-        {formatIDRLocale(100)}
+        {formatIDRLocale(item.opening_balance.debit)}
       </td>
       <td className="px-5 py-1 text-black text-end text-theme-xs dark:text-white whitespace-nowrap border-r dark:border-white/[0.10]">
-        {formatIDRLocale(100)}
+        {formatIDRLocale(item.opening_balance.credit)}
       </td>
       <td className="px-5 py-1 text-black text-end text-theme-xs dark:text-white whitespace-nowrap border-r dark:border-white/[0.10]">
-        {formatIDRLocale(100)}
+        {formatIDRLocale(item.movement_balance.debit)}
       </td>
       <td className="px-5 py-1 text-black text-end text-theme-xs dark:text-white whitespace-nowrap border-r dark:border-white/[0.10]">
-        {formatIDRLocale(100)}
+        {formatIDRLocale(item.movement_balance.credit)}
       </td>
       <td className="px-5 py-1 text-black text-end text-theme-xs dark:text-white whitespace-nowrap border-r dark:border-white/[0.10]">
-        {formatIDRLocale(100)}
+        {formatIDRLocale(item.closing_balance.debit)}
       </td>
       <td className="px-5 py-1 text-black text-end text-theme-xs dark:text-white whitespace-nowrap border-r dark:border-white/[0.10]">
-        {formatIDRLocale(100)}
+        {formatIDRLocale(item.closing_balance.credit)}
       </td>
     </tr>
   );
--- src/modules/user/report/business/TrialBalance/Table/index.tsx
+++ src/modules/user/report/business/TrialBalance/Table/index.tsx
@@ -1,158 +1,152 @@
-// import { BeatLoader } from "react-spinners";
-import TableHeader from "./TableHeader";
-import { formatIDRLocale } from "@helpers/currency";
+import TableAction from "./TableAction";
 import TableItem from "./TableItem";
 
+import { formatIDRLocale } from "@helpers/currency";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHeader,
+  TableLoading,
+  TableNotFound,
+  TableRow,
+} from "@components/ui/table";
+import useGetAllTrialBalance from "@services/user/report/trial-balance/hooks/useGetAllTrialBalance";
+import { isEmpty } from "lodash";
+
 export default function RBTrialBalance() {
+  const { data, loading, setSearch } = useGetAllTrialBalance();
   return (
     <>
       <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] lg:p-6">
         <div className="space-y-6">
-          <TableHeader setSearch={(e) => console.log(e)} />
+          <TableAction setSearch={(e) => setSearch(e)} />
 
           <div className="overflow-hidden rounded-xl border border-gray-200 bg-white dark:border-white/[0.05] dark:bg-white/[0.03]">
             <div className="max-w-full overflow-x-auto">
-              <table className="w-full">
-                <thead className="px-6 py-3.5 border-t border-gray-100 border-y bg-gray-50 dark:border-white/[0.05] dark:bg-white/[0.05]">
-                  <tr>
-                    <th
+              <Table className="w-full">
+                <TableHeader className="px-6 py-3.5 border-t border-gray-100 border-y bg-gray-50 dark:border-white/[0.05] dark:bg-white/[0.05]">
+                  <TableRow>
+                    <TableCell
                       colSpan={2}
                       rowSpan={2}
-                      className="pl-5 pt-2 pb-1 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400 border-r align-middle dark:border-white/[0.10]"
+                      isHeader
+                      className="border-r align-middle dark:border-white/[0.10]"
                     >
                       Daftar Akun
-                    </th>
-                    <th
+                    </TableCell>
+                    <TableCell
                       colSpan={2}
-                      className="px-5 pt-2 pb-1 font-medium text-gray-500 text-center text-theme-xs dark:text-gray-400 border-r border-b dark:border-white/[0.10]"
+                      isHeader
+                      className="border-r border-b dark:border-white/[0.10]"
                     >
                       Saldo Awal
-                    </th>
-                    <th
+                    </TableCell>
+                    <TableCell
                       colSpan={2}
-                      className="px-5 pt-2 pb-1 font-medium text-gray-500 text-center text-theme-xs dark:text-gray-400 border-r border-b dark:border-white/[0.10]"
+                      isHeader
+                      className="border-r border-b dark:border-white/[0.10]"
                     >
                       Pergerakan
-                    </th>
-                    <th
+                    </TableCell>
+                    <TableCell
                       colSpan={2}
-                      className="px-5 pt-2 pb-1 font-medium text-gray-500 text-center text-theme-xs dark:text-gray-400 border-r border-b dark:border-white/[0.10]"
+                      isHeader
+                      className="border-r border-b dark:border-white/[0.10]"
                     >
                       Saldo Akhir
-                    </th>
-                  </tr>
-                  <tr>
-                    <th className="px-5 pb-2 pt-1 font-medium text-gray-500 text-center text-theme-xs dark:text-gray-400 border-r dark:border-white/[0.10]">
-                      Debit
-                    </th>
-                    <th className="px-5 pb-2 pt-1 font-medium text-gray-500 text-center text-theme-xs dark:text-gray-400 border-r dark:border-white/[0.10]">
-                      Kredit
-                    </th>
-                    <th className="px-5 pb-2 pt-1 font-medium text-gray-500 text-center text-theme-xs dark:text-gray-400  border-r dark:border-white/[0.10]">
-                      Debit
-                    </th>
-                    <th className="px-5 pb-2 pt-1 font-medium text-gray-500 text-center text-theme-xs dark:text-gray-400  border-r dark:border-white/[0.10]">
-                      Kredit
-                    </th>
-                    <th className="px-5 pb-2 pt-1 font-medium text-gray-500 text-center text-theme-xs dark:text-gray-400  border-r dark:border-white/[0.10]">
-                      Debit
-                    </th>
-                    <th className="px-5 pb-2 pt-1 font-medium text-gray-500 text-center text-theme-xs dark:text-gray-400  border-r dark:border-white/[0.10]">
-                      Kredit
-                    </th>
-                  </tr>
-                </thead>
-
-                <tbody className="divide-y divide-x divide-gray-100 dark:divide-white/[0.05]">
-                  {/* <tr>
-                    <td colSpan={8} className="text-center py-16">
-                      <div className="sweet-loading">
-                        <BeatLoader color="var(--color-brand-600)" />
-                      </div>
-                    </td>
-                  </tr> */}
-                  {Array.from({ length: 6 }).map((_, index) => {
-                    const number = ++index;
-                    return (
-                      <TableItem
-                        key={`table-item-trial-balance-${index}`}
-                        nomor={number}
-                      />
-                    );
-                  })}
-
-                  {/* {loading ? (
-                    <tr>
-                      <td colSpan={5} className="text-center py-16">
-                        <div className="sweet-loading">
-                          <BeatLoader color="var(--color-brand-600)" />
-                        </div>
-                      </td>
-                    </tr>
-                  ) : isEmpty(data) || !data ? (
-                    <tr>
-                      <td colSpan={5} className="text-center py-4">
-                        Data tidak tersedia
-                      </td>
-                    </tr>
-                  ) : (
-                    <>
-                      {data.map((item, index) => {
-                        return (
-                          <TableItem
-                            key={`table-account-${index}`}
-                            item={item}
-                          />
-                        );
-                      })}
-
-                      <tr>
-                        <td
-                          colSpan={2}
-                          className="px-5 py-1 text-black text-end text-theme-xs dark:text-white font-semibold"
-                        >
-                          Total Keseluruhan
-                        </td>
-                        <td className="px-5 py-1 text-black text-end text-theme-xs dark:text-white whitespace-nowrap font-semibold">
-                          {formatIDRLocale(grandTotalDebit)}
-                        </td>
-                        <td className="px-5 py-1 text-black text-end text-theme-xs dark:text-white whitespace-nowrap font-semibold">
-                          {formatIDRLocale(grandTotalCredit)}
-                        </td>
-                      </tr>
-                    </>
-                  )} */}
-                </tbody>
-                <tfoot className="border-t dark:border-white/[0.10]">
-                  <tr>
-                    <td
-                      colSpan={2}
-                      className="pl-5 py-1 text-black text-start text-theme-xs dark:text-white whitespace-nowrap font-semibold"
+                    </TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell
+                      isHeader
+                      className="text-center border-r dark:border-white/[0.10]"
                     >
-                      Total
-                    </td>
+                      Debit
+                    </TableCell>
+                    <TableCell
+                      isHeader
+                      className="text-center border-r dark:border-white/[0.10]"
+                    >
+                      Kredit
+                    </TableCell>
+                    <TableCell
+                      isHeader
+                      className="text-center border-r dark:border-white/[0.10]"
+                    >
+                      Debit
+                    </TableCell>
+                    <TableCell
+                      isHeader
+                      className="text-center border-r dark:border-white/[0.10]"
+                    >
+                      Kredit
+                    </TableCell>
+                    <TableCell
+                      isHeader
+                      className="text-center border-r dark:border-white/[0.10]"
+                    >
+                      Debit
+                    </TableCell>
+                    <TableCell
+                      isHeader
+                      className="text-center border-r dark:border-white/[0.10]"
+                    >
+                      Kredit
+                    </TableCell>
+                  </TableRow>
+                </TableHeader>
 
-                    <td className="px-5 py-1 text-black text-end text-theme-xs dark:text-white whitespace-nowrap">
-                      {formatIDRLocale(100)}
-                    </td>
-                    <td className="px-5 py-1 text-black text-end text-theme-xs dark:text-white whitespace-nowrap">
-                      {formatIDRLocale(100)}
-                    </td>
-                    <td className="px-5 py-1 text-black text-end text-theme-xs dark:text-white whitespace-nowrap">
-                      {formatIDRLocale(100)}
-                    </td>
-                    <td className="px-5 py-1 text-black text-end text-theme-xs dark:text-white whitespace-nowrap">
-                      {formatIDRLocale(100)}
-                    </td>
-                    <td className="px-5 py-1 text-black text-end text-theme-xs dark:text-white whitespace-nowrap">
-                      {formatIDRLocale(100)}
-                    </td>
-                    <td className="px-5 py-1 text-black text-end text-theme-xs dark:text-white whitespace-nowrap">
-                      {formatIDRLocale(100)}
-                    </td>
-                  </tr>
-                </tfoot>
-              </table>
+                <TableBody>
+                  {loading ? (
+                    <TableLoading colSpan={8} />
+                  ) : isEmpty(data) || !data ? (
+                    <TableNotFound colSpan={8} />
+                  ) : (
+                    data?.accounts.map((item, index) => {
+                      return (
+                        <TableItem
+                          key={`table-item-trial-balance-${index}`}
+                          item={item}
+                        />
+                      );
+                    })
+                  )}
+                </TableBody>
+
+                {data?.total && (
+                  <tfoot className="border-t dark:border-white/[0.10]">
+                    <tr>
+                      <td
+                        colSpan={2}
+                        className="pl-5 py-1 text-black text-start text-theme-xs dark:text-white whitespace-nowrap font-semibold"
+                      >
+                        Total
+                      </td>
+
+                      <td className="px-5 py-1 text-black text-end text-theme-xs dark:text-white whitespace-nowrap">
+                        {formatIDRLocale(data.total.opening_balance.debit)}
+                      </td>
+                      <td className="px-5 py-1 text-black text-end text-theme-xs dark:text-white whitespace-nowrap">
+                        {formatIDRLocale(data.total.opening_balance.credit)}
+                      </td>
+                      <td className="px-5 py-1 text-black text-end text-theme-xs dark:text-white whitespace-nowrap">
+                        {formatIDRLocale(data.total.movement_balance.debit)}
+                      </td>
+                      <td className="px-5 py-1 text-black text-end text-theme-xs dark:text-white whitespace-nowrap">
+                        {formatIDRLocale(data.total.movement_balance.credit)}
+                      </td>
+                      <td className="px-5 py-1 text-black text-end text-theme-xs dark:text-white whitespace-nowrap">
+                        {formatIDRLocale(data.total.closing_balance.debit)}
+                      </td>
+                      <td className="px-5 py-1 text-black text-end text-theme-xs dark:text-white whitespace-nowrap">
+                        {formatIDRLocale(data.total.closing_balance.credit)}
+                      </td>
+                    </tr>
+                  </tfoot>
+                )}
+              </Table>
             </div>
           </div>
         </div>
