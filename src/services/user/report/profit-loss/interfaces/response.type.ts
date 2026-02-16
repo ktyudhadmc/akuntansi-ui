6b252713365ce78466bf4f94fdf06cb6c454b2b0
ktyudha <ktyudha@gmail.com>
2026-02-16 14:48:39 +0700

    feat: profit loss item

---
+++ src/services/user/report/profit-loss/interfaces/response.type.ts
@@ -0,0 +1,17 @@
+export interface IGetAllCashFlowResponse {
+  status?: string;
+  message?: string;
+  data: ProfitLoss[];
+}
+
+/** profit loss core */
+export interface ProfitLoss {
+  type: string;
+  amount: number;
+  child: ProfitLossItem[];
+}
+
+export interface ProfitLossItem {
+  name: string;
+  amount: number;
+}
